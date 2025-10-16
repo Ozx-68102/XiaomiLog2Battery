d13d7f23be55411fc8bfb1f86ea6fe190d6f7283
Ozx-68102 <ozx1072960726@gmail.com>
2025-10-16 21:36:53 +0800

    refactor: restructure the PackageCheckers code to enable it to install a specified version of the package

--- Modules/Core/PackageCheckers.py
+++ Modules/Core/PackageCheckers.py
@@ -2,35 +2,86 @@ import subprocess
 import sys
 
 
-def check_and_install_packages():
-    # TODO: dash-uploader version: 0.7.0a2 prerelease
-    required_packages = [
-        "pyecharts", "pandas", "plotly", "dash", "dash-uploader", "dash-bootstrap-components", "json"
-    ]
-    count = 0
-    for package in required_packages:
+def get_package_version(package: str) -> str | bool:
+    """
+    Check package version.
+    :param package: package name
+    :return: Installed => str, Cannot find version => True, Not installed => False
+    """
+    try:
+        result = subprocess.run(args=[sys.executable, "-m", "pip", "show", package], capture_output=True, text=True, check=True)
+        for line in result.stdout.split("\n"):
+            if line.startswith("Version"):
+                return line.split(":")[1].strip()
+
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def install_package(package: str, version: str | None = None) -> None:
+    name = f"{package}=={version}" if version else package
+    args = [sys.executable, "-m", "pip", "install", name]
+    subprocess.check_call(args=args)
+
+
+def check_and_install_packages() -> None:
+    required_packages = {
+        "pandas": None,
+        "plotly": None,
+        "dash": None,
+        "dash-uploader": "0.7.0a2",
+        "dash-bootstrap-components": None
+    }
+
+    counts = 0
+    success = 0
+    failed = 0
+
+    for required_package, specified_version in required_packages.items():
         try:
-            __import__(package)
-            print(f"{count + 1}. {package} is already installed.")
-        except ImportError:
-            try:
-                result = subprocess.run(args=[sys.executable, "-m", "pip", "show", package], stdout=subprocess.DEVNULL,
-                                        stderr=subprocess.DEVNULL)
-                if result.returncode == 0:
-                    print(f"{count + 1}. {package} is already installed.")
-                else:
-                    raise subprocess.CalledProcessError(result.returncode, result.args)
-            except subprocess.CalledProcessError:
-                try:
-                    print(f"{count + 1}. {package} is not installed. So installation will begin soon...")
-                    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-                except subprocess.CalledProcessError:
-                    print(f"Failed to install {package}. Please check your internet connection or Python environment.")
-                    sys.exit(1)
+            if not specified_version:
+                status = get_package_version(required_package)
 
-        count += 1
+                # Package installed
+                if isinstance(status, str) or status is True:
+                    print(f"{counts + 1}. {required_package} is already installed.")
+                    counts += 1
+                    success += 1
+                    continue
 
-    sys.exit(0)
+                # Package not install
+                print(f"{counts + 1}. {required_package} is not installed. So installation will begin soon...")
+                install_package(required_package)
+                counts += 1
+                success += 1
+                continue
+
+            # Specified package version
+            version = get_package_version(required_package)
+
+            # Version incorrect
+            if version != specified_version:
+                print(f"{counts + 1}. {required_package} version incorrect or not installed. So installation will begin soon...")
+                install_package(required_package, specified_version)
+                counts += 1
+                success += 1
+                continue
+
+            # Version correct
+            print(f"{counts + 1}. {required_package} is already installed.")
+            counts += 1
+            success += 1
+            continue
+        except subprocess.CalledProcessError as e:
+            print(f"{counts + 1}. Failed to install {required_package}: {e}")
+            counts += 1
+            failed += 1
+            continue
+
+    print(f"Completed. Success: {success}. Failed: {failed}. Total: {counts}.")
+    exit_code = 1 if failed > 0 else 0
+    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
