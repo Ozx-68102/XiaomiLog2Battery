5203277f6786aac0a8bc45aff8a48cdc39f10f8c
Ozx-68102 <ozx1072960726@gmail.com>
2025-10-17 15:05:59 +0800

    feat: add a very basic status prompt

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -9,8 +9,6 @@ from Modules.Core import init_graph
 from Modules.FileProcess import INSTANCE_PATH
 
 basepath = os.path.dirname(os.path.abspath(__file__))
-save_path = os.path.join(INSTANCE_PATH, "upload")
-os.makedirs(save_path, exist_ok=True)
 
 app = dash.Dash(__name__, assets_folder=os.path.join(basepath, "assets"))
 app.config.suppress_callback_exceptions = True
@@ -52,13 +50,65 @@ app.layout = dbc.Container([
         ], width=12),
         className="mb-4"
     ),
-    html.Div(id="output-container", style={"display": "flex", "flexDirection": "column", "gap": "10px"}),
+    dbc.Row(html.Div(id="status-container")),
+    dbc.Row(html.Div(id="output-container", style={"display": "flex", "flexDirection": "column", "gap": "10px"}))
 ], fluid=True, style={"padding": "15px"})
 
 
-@du.callback(output=Output("output-container", "children"), id="upload-component")
-def handle_upload(status: du.UploadStatus) -> html.Div:
-    return init_graph(status=status)
+@du.callback(
+    output=[
+        Output("status-container", "children"),
+        Output("output-container", "children"),
+    ],
+    id="upload-component"
+)
+def handle_upload(status: du.UploadStatus) -> tuple[dbc.Alert | html.Div, dbc.Alert | html.Div]:
+    # TODO: This function will be called while a file was uploaded.
+    #  Not begin uploading, so the prompt won't appear immediately.
+    #  If upload failed, it will not be called.
+    if not status.is_completed:
+        file_count = len(status.uploaded_files)
+        if file_count < 1:
+            status_message = [html.P("Now files are uploading, it may take some time, so hang tight.")]
+        elif file_count == 1:
+            filename = [os.path.basename(os.fspath(fp)) for fp in status.uploaded_files][0]
+            status_message = [
+                html.P("Files still uploading..."),
+                html.P("System detected that 1 file was uploaded successfully:"),
+                html.Ul([html.Li(filename)])
+            ]
+        else:
+            filenames = [os.path.basename(os.fspath(fp)) for fp in status.uploaded_files]
+            status_message = [
+                html.P("Files still uploading..."),
+                html.P(f"System detected that {file_count} files were uploaded successfully:"),
+                html.Ul([html.Li(fn) for fn in filenames])
+            ]
+
+        status_display = dbc.Alert([
+            html.H3("Uploading..."),
+            html.Hr(),
+            html.Div(status_message)
+        ], color="info", dismissable=False)
+        return status_display, html.Div()
+
+
+    filepath = [os.fspath(fp) for fp in status.uploaded_files]
+    charts = init_graph(filepath_list=filepath)
+    if not charts:
+        status_display = dbc.Alert([
+            html.H3("Oops! An unexpected error occurred"),
+            html.Hr(),
+            html.Div("Failed to generate Graphs.")
+        ], color="danger", dismissable=False)
+        return status_display, html.Div()
+
+    status_display = dbc.Alert([
+        html.H3("Successful!"),
+        html.Hr(),
+        html.Div("The graphs were generated successfully below.")
+    ])
+    return status_display, charts
 
 
 if __name__ == "__main__":
--- Modules/Core/Backend.py
+++ Modules/Core/Backend.py
@@ -1,6 +1,3 @@
-import os
-
-import dash_uploader as du
 from dash import html, dcc
 
 from Modules.DataAnalysis import BatteryInfoParser, BatteryDataService, PlotlyVisualizer
@@ -12,26 +9,23 @@ _data_service = BatteryDataService()
 _visualizer = PlotlyVisualizer()
 
 
-def init_graph(status: du.UploadStatus) -> html.Div:
-    print(f"Is completed: {status.is_completed}")
-    if not status.is_completed:
-        return html.Div()
-
+def init_graph(filepath_list: list[str]) -> html.Div | None:
     print("Uploaded successfully.")
-    uploaded_files = [os.fspath(fp) for fp in status.uploaded_files]
 
     print("Start to process zip file(s).")
-    txt_list = _log_processor.process_xiaomi_log(fps=uploaded_files)
+    txt_list = _log_processor.process_xiaomi_log(fps=filepath_list)
     print(f"Done with {len(txt_list)} txt file(s).")
 
     print("Start to parse txt file(s).")
     battery_info = _info_parser.parse_battery_info(tps=txt_list)
     print(f"Done with {len(battery_info)} battery info.")
 
+    # TODO: Here and database end need to set a `TRUNCATE` logic to adapt to `initial graph` function.
+    #  Otherwise it will throw Database Error when use it multiple time
     print("Start to save battery info to database.")
     if not _data_service.save_battery_data(data=battery_info):
         print("Failed to save battery data.")
-        return html.Div()
+        return None
 
     print("Start to visualize needed graph.")
     vis_data = _data_service.get_all_battery_data()
